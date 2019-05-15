9a6f6983615d7888ff095fe61551991a22444b9c
Kevin <kevinabram1000@gmail.com>
2019-05-15 12:16:32 +0700

    Initial Commit

---
+++ object_orient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  object_orient
+//
+//  Created by Kevin Abram on 15/05/19.
+//  Copyright © 2019 Kevin Abram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
